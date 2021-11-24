7259665b9fbf499b640b29ddb8d7340ad8b6b4e9
yuliasemenyuk <semenyukyp@gmail.com>
2021-11-25 00:20:14 +0200

    corrr

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,22 +13,25 @@ function createPromise(position, delay) {
   });
 }
 
-form.addEventListener('submit', (event) => {
-  
-  event.preventDefault();
-  let delay = Number(form.elements.delay.value);
-  let step = Number(form.elements.step.value);
-  let amount = form.elements.amount.value;
-  let position = 1;
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    delay += step;
-    position += 1;
-  }
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+let delay = Number(form.elements.delay.value);
+let step = Number(form.elements.step.value);
+let amount = form.elements.amount.value;
+let position = 1;
+for (let i = 1; i <= amount; i += 1) {
+  createPromise(position, delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  delay += step;
+  position += 1;
+};
 }
+  
